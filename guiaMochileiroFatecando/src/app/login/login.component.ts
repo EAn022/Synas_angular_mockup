7658fa7727d2b5828756b89e6512c9bb0e01729d
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2024-10-06 11:18:56 -0300

    add: form to login page

--- guiaMochileiroFatecando/src/app/login/login.component.ts
+++ guiaMochileiroFatecando/src/app/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoginFormComponent } from '../login-form/login-form.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [ 
-    RouterModule
+    RouterModule,
+    LoginFormComponent
    ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
