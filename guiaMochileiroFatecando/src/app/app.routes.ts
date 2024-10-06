39e4095f3231211749913e54911ca2c1d271834e
root <root@LAPTOP-FIM9VNFE>
2024-10-06 10:59:16 -0300

    add: routes between pages

--- guiaMochileiroFatecando/src/app/app.routes.ts
+++ guiaMochileiroFatecando/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'home', component: HomeComponent},
 ];
