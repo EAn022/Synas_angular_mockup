c3980e44efacee3a0446dc7b5b4c26cc1133ee2b
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2024-10-07 17:15:38 -0300

    update: navbar

--- guiaMochileiroFatecando/src/app/home/home.component.ts
+++ guiaMochileiroFatecando/src/app/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    NavbarComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
