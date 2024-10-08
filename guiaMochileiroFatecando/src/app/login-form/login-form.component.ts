b05e3cdd70bbf4e3c68a9f747523a456fab4bba9
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2024-10-08 16:45:12 -0300

    add: togle visibility in the password inputs

--- guiaMochileiroFatecando/src/app/login-form/login-form.component.ts
+++ guiaMochileiroFatecando/src/app/login-form/login-form.component.ts
@@ -11,5 +11,20 @@ import { RouterModule } from '@angular/router';
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
+  
+  isPasswordVisible: boolean = false;
+  passwordIcon: string = 'assets/icons/lock-close-icon.svg';
 
+  // Função para alternar a visibilidade da senha
+  togglePasswordVisibility(): void {
+    this.isPasswordVisible = !this.isPasswordVisible;
+    this.passwordIcon = this.isPasswordVisible 
+      ? 'assets/icons/lock-open-icon.svg'  // Ícone de cadeado aberto
+      : 'assets/icons/lock-close-icon.svg'; // Ícone de cadeado fechado
+  }
+
+  // Função para retornar o tipo do input
+  getPasswordInputType(): string {
+    return this.isPasswordVisible ? 'text' : 'password';
+  }
 }
