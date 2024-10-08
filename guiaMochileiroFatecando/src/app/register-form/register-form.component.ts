5200c048a42f6ccb304a259c736cdba1f70b7eaf
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2024-10-08 16:48:11 -0300

    update: togle visibility in the password inputs

--- guiaMochileiroFatecando/src/app/register-form/register-form.component.ts
+++ guiaMochileiroFatecando/src/app/register-form/register-form.component.ts
@@ -11,20 +11,37 @@ import { RouterModule } from '@angular/router';
   styleUrl: './register-form.component.scss'
 })
 export class RegisterFormComponent {
-  isPasswordVisible: boolean = false;
-  passwordIcon: string = 'assets/icons/lock-close-icon.svg';
 
-  // Função para alternar a visibilidade da senha
-  togglePasswordVisibility(): void {
-    this.isPasswordVisible = !this.isPasswordVisible;
-    this.passwordIcon = this.isPasswordVisible 
-      ? 'assets/icons/lock-open-icon.svg'  // Ícone de cadeado aberto
-      : 'assets/icons/lock-close-icon.svg'; // Ícone de cadeado fechado
+  // Controla a visibilidade da primeira e segunda senha
+  isPasswordVisible1: boolean = false;
+  isPasswordVisible2: boolean = false;
+
+  // Ícones das senhas
+  passwordIcon1: string = 'assets/icons/lock-close-icon.svg';
+  passwordIcon2: string = 'assets/icons/lock-close-icon.svg';
+
+  // Função para alternar a visibilidade das senhas de acordo com o campo (1 ou 2)
+  togglePasswordVisibility(field: number): void {
+    if (field === 1) {
+      this.isPasswordVisible1 = !this.isPasswordVisible1;
+      this.passwordIcon1 = this.isPasswordVisible1 
+        ? 'assets/icons/lock-open-icon.svg'  // Ícone de cadeado aberto
+        : 'assets/icons/lock-close-icon.svg'; // Ícone de cadeado fechado
+    } else if (field === 2) {
+      this.isPasswordVisible2 = !this.isPasswordVisible2;
+      this.passwordIcon2 = this.isPasswordVisible2 
+        ? 'assets/icons/lock-open-icon.svg'  // Ícone de cadeado aberto
+        : 'assets/icons/lock-close-icon.svg'; // Ícone de cadeado fechado
+    }
   }
 
-  // Função para retornar o tipo do input
-  getPasswordInputType(): string {
-    return this.isPasswordVisible ? 'text' : 'password';
+  // Função para retornar o tipo do input de acordo com o campo (1 ou 2)
+  getPasswordInputType(field: number): string {
+    if (field === 1) {
+      return this.isPasswordVisible1 ? 'text' : 'password';
+    } else if (field === 2) {
+      return this.isPasswordVisible2 ? 'text' : 'password';
+    }
+    return 'password';
   }
-  
 }
